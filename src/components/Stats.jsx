44709ae92bbbd8ce1b58b250a7959550f83b6acc
lillykml <64362067+lillykml@users.noreply.github.com>
2024-07-30 16:46:31 +0200

    implemented stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,10 +1,15 @@
+import { stats } from '../constants/index';
+
 function Stats() {
   return (
-    <div className="flexStart paddingX">
-      <div className="boxWidth">
-        Stats
-      </div>
-    </div>
+    <section className="flex justify-center items-center flex-wrap paddingX boxWidth">
+      {stats.map(stat => (
+        <div key={stat.id} className='flex justify-start items-center flex-row m-3'>
+          <h4 className='font-semibold text-white text-[30px] sm:text-[40px]'>{stat.value}</h4>
+          <p className="font-normal sm:text-[20px] text-[15px] text-gradient uppercase ml-3">{stat.title}</p>
+        </div>
+      ))}
+    </section>
   )
 }
 
