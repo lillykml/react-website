b993cbbac8e04b83c8b7c9929ee1d10dac3df1f8
lillykml <64362067+lillykml@users.noreply.github.com>
2024-07-30 17:57:59 +0200

    Appstore section

--- src/components/Appstore.jsx
+++ src/components/Appstore.jsx
@@ -1,8 +1,22 @@
-
+import Apple from '../assets/apple.svg'
+import Google from '../assets/google.svg'
+import Billing from '../assets/billing.svg'
 
 function Appstore() {
   return (
-    <div>Appstore</div>
+    <section id='billing' className='section paddingX py-6'>
+      <div className='sectionImg'>
+        <img src={Billing} />
+      </div>
+      <div className='sectionInfo'>
+        <h2>Easily control your <br className='sm:block hidden'/> billing & invoicing.</h2>
+        <p className='max-w-[470px] mt-5'>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <div className='mt-5 space-x-2'>
+          <button><img src={Apple} /></button>
+          <button><img src={Google} /></button>
+        </div>
+      </div>
+    </section>
   )
 }
 
