8bb092465d5756791eeb2fc95e173459e1f31a27
lillykml <64362067+lillykml@users.noreply.github.com>
2024-07-31 15:10:59 +0200

    Format fixes

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,8 @@
 function FeedbackCard({ feedback }) {
   return (
-    <div className="text-white flex flex-col flex-1 rounded-lg p-8 feedback-card justify-between">
-      <h1 className="text-gradient italic font-bold text-[80px]">"</h1>
-      <p className="text-white font-light text-[18px] pb-8">{feedback.content}</p>
+    <div className="text-white flex flex-col flex-1 rounded-lg p-8 feedback-card justify-between h-[395px] w-[370px]">
+      <h1 className="text-gradient italic font-bold text-[80px] mb-0 pb-0">"</h1>
+      <p className="text-white font-light text-[18px] flex-grow">{feedback.content}</p>
       <div className="flex flex-row space-x-4">
         <img src={feedback.img} />
         <div>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,8 +6,8 @@ function Testimonials() {
   return (
     <section id="testimonials" className='section paddingX py-6 flex flex-col'>
       <div className="flex flex-col sm:flex-row items-center">
-      <h2>What people are saying about us</h2>
-        <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        <h2>What people are saying <br className='hidden sm:block'/> about us</h2>
+          <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
       </div>
       <div className="flex flex-col sm:flex-row items-center justify-between">
         {feedback.map(comment => (
@@ -16,7 +16,6 @@ function Testimonials() {
       </div>
       <div className='flex flex-col sm:flex-row items-center justify-between'>
         {clients.map(client => <img className='object-contain w-[190px] p-6' key={client.id} src={client.logo} />)}
-
       </div>
     </section>
   )
