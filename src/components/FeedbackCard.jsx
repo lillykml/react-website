cdbe7a3b760fc1de391ba9b40f8874333ab86c5b
lillykml <64362067+lillykml@users.noreply.github.com>
2024-07-31 11:47:49 +0200

    finished testimonial section

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,16 @@
-
-
-function FeedbackCard() {
+function FeedbackCard({ feedback }) {
   return (
-    <div>FeedbackCard</div>
+    <div className="text-white flex flex-col flex-1 rounded-lg p-8 feedback-card justify-between">
+      <h1 className="text-gradient italic font-bold text-[80px]">"</h1>
+      <p className="text-white font-light text-[18px] pb-8">{feedback.content}</p>
+      <div className="flex flex-row space-x-4">
+        <img src={feedback.img} />
+        <div>
+          <p className="text-white">{feedback.name}</p>
+          <p className="font-light text-[16px]">{feedback.title}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,18 @@
-
+import { feedback } from '../constants/index';
+import FeedbackCard from './FeedbackCard';
 
 function Testimonials() {
   return (
-    <section>
-      
+    <section id="testimonials" className='section paddingX py-6 flex flex-col'>
+      <div className="flex flex-col sm:flex-row items-center">
+      <h2>What people are saying about us</h2>
+        <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+      <div className="flex flex-col sm:flex-row items-center justify-between">
+        {feedback.map(comment => (
+          <FeedbackCard key={comment.id} feedback={comment} />
+        ))}
+      </div>
     </section>
   )
 }
