58be73ed7258026045d8afc0ee002fa44391e559
lillykml <64362067+lillykml@users.noreply.github.com>
2024-07-31 14:27:52 +0200

    updated navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,20 +23,20 @@ const Navbar = () => {
         <nav className='paddingX py-6 text-white flex justify-between items-center'>
             <img src={Logo} alt='logo' className='w-[124px] h-[32px]'/>
             <div className='hidden md:flex space-x-10'>
-                <a href='/'>Home</a>
-                <a href='/'>About Us</a>
-                <a href='/'>Features</a>
-                <a href='/'>Solution</a>
+                <a href='#home'>Home</a>
+                <a href='#aboutus'>About Us</a>
+                <a href="#features" >Features</a>
+                <a href='#footer'>Contact</a>
             </div>
             <div onClick={() => setOpen((prev) => !prev)} className='block md:hidden'>
                     {open ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
             </div>
             <div className={open ? 'fixed left-0 top-16 w-[60%] bg-primary h-full ease-in-out duration-500' : 'fixed left-[-100%]'}>
                 <div className='space-y-3 flex flex-col mt-8 ml-8'>
-                    <a href='/'>Home</a>
-                    <a href='/'>About Us</a>
-                    <a href='/'>Features</a>
-                    <a href='/'>Solution</a>
+                    <a href='#home'>Home</a>
+                    <a href='#aboutus'>About Us</a>
+                    <a href="#features" >Features</a>
+                    <a href='#footer'>Contact</a>
                 </div>
             </div>
         </nav>
